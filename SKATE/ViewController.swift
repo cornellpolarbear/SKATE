0818145c6039286b0c4e3ef50ca1c74e375a838d
cornellpolarbear <addob_000@dhcp-hol-759.redrover.cornell.edu>
2016-09-13 15:17:17 -0400

    Initial code

--- SKATE/ViewController.swift
+++ SKATE/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData : [String] = [String]()
+    
+    @IBOutlet weak var skater1: UIButton!
+    @IBOutlet weak var skater2: UIButton!
+    
+    @IBOutlet weak var skate1: UILabel!
+    @IBOutlet weak var skate2: UILabel!
+    
+    @IBOutlet weak var addLetter1: UIButton!
+    @IBOutlet weak var addLetter2: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        
+        // Input data into the Array:
+        pickerData = ["Ollie", "BS Pop Shuvit", "FS Pop Shuvit", "Kickflip", "Heelflip", "Treflip", "Inward Heelflip", "Hardflip", "BS Bigspin", "FS Bigspin", "BS 360 Pop Shuvit", "FS 360 Pop Shuvit" ].sort()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    // Catpure the picker view selection
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+    }
+    
 }
 
